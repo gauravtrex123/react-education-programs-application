9488ccb9ba4ade12d20003122bcfa3c21e923758
gauravranwatrex@gmail.com <gauravranwatrex@gmail.com>
2020-06-28 14:47:44 +0530

    deployment commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardComponent from './card-component';
 import data from './data.json';
 
 class App extends React.Component {
@@ -9,70 +10,6 @@ class App extends React.Component {
 
     var tempObj = {};
     this.eduProgram = data;
-
-    this.eduProgram.forEach((v) => {
-      tempObj[v.position] = v.position;
-      tempObj[v.position + "ageFrom"] = v.ageFrom;
-      tempObj[v.position + "ageTo"] = v.ageTo;
-      tempObj[v.position + "totalSessions"] = v.totalSessions;
-      tempObj[v.position + "sessionsPerWeek"] = v.sessionsPerWeek;
-      tempObj[v.position + "description"] = v.description
-    });
-
-    this.state = tempObj;
-    this.openChanges = this.openChanges.bind(this);
-    this.saveChanges = this.saveChanges.bind(this);
-  }
-
-  saveChanges(e) {
-    e.persist();
-    console.log(e);
-    e.stopPropagation();
-    console.log('........................');
-    var that = this;
-    document.querySelectorAll('.' + e.target.dataset.id_button + '-input-show').forEach(function (v) {
-      v.style.display = 'none';
-    })
-
-    document.querySelectorAll('.' + e.target.dataset.id_button + '-display-show').forEach(function (v) {
-      v.style.display = 'block';
-    })
-
-    document.querySelector('.' + e.target.dataset.id_button + '-save-button').style.display = 'none';
-    document.querySelector('.' + e.target.dataset.id_button + '-edit-button').style.display = 'block';
-
-    var tempObjSub = {
-      [e.target.dataset.id_button]: document.querySelector('[data-id="' + e.target.dataset.id_button + '"]').value,
-      [e.target.dataset.id_button + "ageFrom"]: document.querySelector('[data-id="' + e.target.dataset.id_button + 'ageFrom"]').value,
-      [e.target.dataset.id_button + "ageTo"]: document.querySelector('[data-id="' + e.target.dataset.id_button + 'ageTo"]').value,
-      [e.target.dataset.id_button + "totalSessions"]: document.querySelector('[data-id="' + e.target.dataset.id_button + 'totalSessions"]').value,
-      [e.target.dataset.id_button + "sessionsPerWeek"]: document.querySelector('[data-id="' + e.target.dataset.id_button + 'sessionsPerWeek"]').value,
-      [e.target.dataset.id_button + "description"]: document.querySelector('[data-id="' + e.target.dataset.id_button + 'description"]').value
-    } 
-    this.setState(tempObjSub); 
-  }
-
-  openChanges(e) {
-    e.persist();
-    console.log(e);
-    console.log('........................');
-    document.querySelectorAll('.' + e.target.dataset.id_parent + '-input-show').forEach(function (v) {
-      v.style.display = 'block';
-    })
-
-    document.querySelectorAll('.' + e.target.dataset.id_parent + '-display-show').forEach(function (v) {
-      v.style.display = 'none';
-    })
-
-    document.querySelector('.' + e.target.dataset.id_parent + '-save-button').style.display = 'block';
-    document.querySelector('.' + e.target.dataset.id_parent + '-edit-button').style.display = 'none';
-
-    document.querySelector('[data-id="' + e.currentTarget.dataset.id_parent + '"]').value = this.state[e.currentTarget.dataset.id_parent];
-    document.querySelector('[data-id="' + e.currentTarget.dataset.id_parent + 'ageFrom"]').value = this.state[e.currentTarget.dataset.id_parent + "ageFrom"];
-    document.querySelector('[data-id="' + e.currentTarget.dataset.id_parent + 'ageTo"]').value = this.state[e.currentTarget.dataset.id_parent + "ageTo"];
-    document.querySelector('[data-id="' + e.currentTarget.dataset.id_parent + 'totalSessions"]').value = this.state[e.currentTarget.dataset.id_parent + "totalSessions"];
-    document.querySelector('[data-id="' + e.currentTarget.dataset.id_parent + 'sessionsPerWeek"]').value = this.state[e.currentTarget.dataset.id_parent + "sessionsPerWeek"];
-    document.querySelector('[data-id="' + e.currentTarget.dataset.id_parent + 'description"]').value = this.state[e.currentTarget.dataset.id_parent + "description"];
   }
 
   render() {
@@ -81,31 +18,7 @@ class App extends React.Component {
         {
           this.eduProgram.map((v, i) => {
             return (
-              <div className="data-container" key={v.position + i}>
-                <div className="course-image"><img src={v.imgCourse} /></div>
-                <div className="second-container">
-                  <div className="position-age-container">
-                    <div className="position-container">
-                      <div className={"position-display display-show " + v.position + "-display-show"}>{this.state[v.position]}</div>
-                      <input className={"position-input input-show " + v.position + "-input-show"} data-id={v.position} />
-                    </div>
-                    <div className="age-container">
-                      <div className={"age-display display-show " + v.position + "-display-show"}>Age {this.state[v.position + "ageFrom"]} - {this.state[v.position + "ageTo"]}</div>
-                      <div className={"age-input input-show " + v.position + "-input-show"}>Age <input className="agefrom-input" data-id={v.position + "ageFrom"} /> - <input className="ageto-input" data-id={v.position + "ageTo"} /></div>
-                    </div>
-                  </div>
-                  <div className="weeks-container">
-                    <div className={"weeks-display display-show " + v.position + "-display-show"}>{this.state[v.position + "totalSessions"]} sessions | {this.state[v.position + "sessionsPerWeek"]} per week</div>
-                    <div className={"age-input input-show " + v.position + "-input-show"}><input className="totalsessions-input" data-id={v.position + "totalSessions"} /> sessions | <input className="sessionsperweek-input" data-id={v.position + "sessionsPerWeek"} /> per week</div>
-                  </div>
-                  <div className="description-container">
-                    <div className={"description-display display-show " + v.position + "-display-show"}>{this.state[v.position + "description"]}</div>
-                    <div className={"description-input input-show " + v.position + "-input-show"}><input data-id={v.position + "description"} /></div>
-                  </div>
-                  <div className="button-container"><button className={"edit-button " + v.position + "-edit-button"} data-id_parent={v.position} onClick={((e) => this.openChanges(e))}>Edit</button>
-                    <button className={"save-button " + v.position + "-save-button"} data-id_button={v.position} onClick={((e) => this.saveChanges(e))}>Save</button></div>
-                </div>
-              </div>
+              <CardComponent value={v}/>
             )
           })
         }
---
+++ src/card-component.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import './App.css';
+
+class CardComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            position: props.value.position,
+            imgCourse: props.value.imgCourse,
+            ageFrom: props.value.ageFrom,
+            ageTo: props.value.ageTo,
+            totalSessions: props.value.totalSessions,
+            sessionsPerWeek: props.value.sessionsPerWeek,
+            description: props.value.description,
+            mode: 'display'
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.changeMode = this.changeMode.bind(this);
+    }
+
+    handleInputChange(event) {
+        event.preventDefault(); 
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    changeMode(event){
+        event.preventDefault();
+        this.setState({
+           mode:event.target.name
+        });
+    }
+
+    render() {
+        return (
+            <div className="data-container" key={this.state.position}>
+                <div className="course-image"><img src={this.state.imgCourse} /></div>
+                <div className="second-container">
+                    <form>
+                        <div className="position-age-container">
+                            <div className="position-container">
+                                {this.state.mode == 'display' ?
+                                    <div className="position-display display-show">{this.state.position}</div>
+                                    :
+                                    <input className="position-input input-show" name="position" defaultValue={this.state.position} onChange={this.handleInputChange} />
+                                }
+                            </div>
+                            <div className="age-container">
+                                {this.state.mode == 'display' ?
+                                    <div className="age-display display-show">Age {this.state.ageFrom} - {this.state.ageTo}</div>
+                                    :
+                                    <div className="age-input input-show">Age <input className="agefrom-input" name="ageFrom" defaultValue={this.state.ageFrom} onChange={this.handleInputChange} /> - <input className="ageto-input" name="ageTo" defaultValue={this.state.ageTo} onChange={this.handleInputChange} /></div>
+                                }
+                            </div>
+                        </div>
+                        <div className="weeks-container">
+                            {this.state.mode == 'display' ?
+                                <div className="weeks-display display-show">{this.state.totalSessions} sessions | {this.state.sessionsPerWeek} per week</div>
+                                :
+                                <div className="age-input input-show"><input className="totalsessions-input" name="totalSessions" defaultValue={this.state.totalSessions} onChange={this.handleInputChange} /> sessions | <input className="sessionsperweek-input" name="sessionsPerWeek" defaultValue={this.state.sessionsPerWeek} onChange={this.handleInputChange} /> per week</div>
+                            }
+                        </div>
+                        <div className="description-container">
+                            {this.state.mode == 'display' ?
+                                <div className="description-display display-show">{this.state.description}</div>
+                                :
+                                <div className="description-input input-show"><input name="description" defaultValue={this.state.description} onChange={this.handleInputChange} /></div>
+                            }
+                        </div>
+                        <div className="button-container">
+                            {this.state.mode == 'display' ?
+                                <button className="edit-button" name="edit" onClick={this.changeMode}>Edit</button>
+                                :
+                                <button className="save-button" name="display" onClick={this.changeMode}>Save</button>
+                            }
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default CardComponent;
